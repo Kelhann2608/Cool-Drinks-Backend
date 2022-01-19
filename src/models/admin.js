1d2ee34aa9494c7f6562043b8b1a147724f54f27
Kelhann2608 <bernardin2608@gmail.com>
2022-01-19 10:40:35 +0100

    added admins and auth files in controllers and admin file in helpers

--- src/models/admin.js
+++ src/models/admin.js
@@ -1,3 +1,49 @@
-import connection from "../db-config";
-import Joi from 'joi';
-import argon2 from 'argon2';
+const connection = require('../db-config');
+const argon = require('argon2');
+const Joi = require('joi');
+
+const hashOptions = {
+    type: argon.argon2id,
+    memoryCost: 2 ** 16,
+    timeCost: 5,
+    parallelism: 1,
+  };
+
+  const validate = (data) => {
+    return Joi.object({
+      lastname: Joi.string().max(100).required(),
+      firstname: Joi.string().max(100).required(),
+      email: Joi.string().max(100).required(),
+      password: Joi.string().min(7).max(20).required(),
+    }).validate(data, { abortEarly: false }).error;
+  };
+
+  const cryptePassword = (password) => {
+    return argon.hash(password, hashOptions);
+  };
+
+  const verifyPassword = (password, cryptedPassword) => {
+    return argon.verify(cryptedPassword, password, hashOptions);
+  };
+
+  const create = (lastname, firstname, email, password) => {
+    return connection
+    .promise()
+    .query(
+      'INSERT INTO admins(lastname, firstname, email, password) VALUES (?,?,?,?)',
+      [lastname, firstname, email, password]
+    );
+  };
+
+  const getByEmail = (email) => {
+    return connection
+    .promise()
+    .query('SELECT * FROM admins WHERE email = ?', [email]);
+  };
+
+  module.exports = {
+    cryptePassword,
+    validate,
+    verifyPassword,
+    create,getByEmail,
+  };
